f4ce595de9d9b3f86a6944400f6debece607c76a
JayMackay <jared.mackay1994@live.com>
2021-05-04 14:33:34 +0100

    Initial Firebase & React Base Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB4Fy6NOTo7ZuymSYm3_rCXzHoh1pz6aeM",
+    authDomain: "store-clone-c754c.firebaseapp.com",
+    projectId: "store-clone-c754c",
+    storageBucket: "store-clone-c754c.appspot.com",
+    messagingSenderId: "569357693282",
+    appId: "1:569357693282:web:660caa25a977024c1fbc3b",
+    measurementId: "G-2P2B52ZYSZ"
+  };
